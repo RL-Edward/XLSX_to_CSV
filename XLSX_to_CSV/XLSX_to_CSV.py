b25de501a5886a1b89919fd1f557805223eddfa6
Ruben Lopez <rubenedward@outlook.com>
2019-11-20 09:46:19 -0600

    Added a new function to help clean up output csvs. Current Status: Operational Details: Added __rename_duplicate_columns to rename duplicate column names that would have caused issues during CQ ingestion. With this addition, duplicate (abs) columns will be renamed to a combination of the previous column name and (abs).

--- XLSX_to_CSV/XLSX_to_CSV.py
+++ XLSX_to_CSV/XLSX_to_CSV.py
@@ -17,7 +17,6 @@ class XLSX_to_CSV:
             jobs = OrderedDict()
             jobs[cls.__convert_csv] = []
             for xlsx_file_name in xlsx_files:
-                
                 jobs[cls.__convert_csv].append({"File Path": os.path.join(path, xlsx_file_name)})   
             Threader.threads(jobs)                
         else:
@@ -30,6 +29,7 @@ class XLSX_to_CSV:
         try:
             frame = pd.read_excel(file_path, index_col=None)
             frame = cls.__remove_invalid_characters(frame)
+            frame = cls.__rename_duplicate_columns(frame)
             file_path = file_path.replace(".xlsx", ".csv")
             frame.to_csv(file_path, encoding='utf-8', index=False)
             print("Converted %s Successfully." % file_name)
@@ -41,6 +41,18 @@ class XLSX_to_CSV:
         for header in frame.columns.values:
             frame.rename(columns={header:header.replace(u"\u2122", '')}, inplace=True)
         return frame
+
+    @classmethod
+    def __rename_duplicate_columns(cls, frame):
+        prev_col_name = None
+        for col_name in list(frame.columns.values):
+            if "(abs)" in col_name:
+                frame.rename(columns={col_name:prev_col_name + " (abs)"}, inplace=True)
+            prev_col_name = col_name 
+        
+        return frame
+
+
 if __name__ == "__main__":
     try:
         path = sys.argv[1]
